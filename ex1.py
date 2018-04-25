8784ea70ecfaba0b19889185b67825082d00ea36
root <manuelenriquesantosiglesias@gmail.com>
2018-04-25 02:07:44 +0200

    primer commit

---
+++ ex1.py
@@ -0,0 +1,30 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.applications import inception_v3
+
+# Carga el modelo
+model = inception_v3.InceptionV3()
+
+# Cargamos la imagen a un array
+img = image.load_img("pelican.jpg", target_size=(300, 300))
+img2 = image.load_img("pelican2.jpg", target_size=(300, 300))
+img3 = image.load_img("pelican3.jpg", target_size=(300, 300))
+
+input_image = image.img_to_array(img)
+
+# Escalamos la imagen
+input_image /= 255.
+input_image -= 0.5
+input_image *= 2.
+
+# Ponemos la imagen como keras espera
+input_image = np.expand_dims(input_image, axis=0)
+
+# Ejecutamos la predicción
+predictions = model.predict(input_image)
+
+# Imprimimos en pantalla la predicción
+predicted_classes = inception_v3.decode_predictions(predictions, top=5)
+print(predicted_classes)
+imagenet_id, name, confidence = predicted_classes[0][0]
+print("!Es {} con {:.4}% de probabilidad!".format(name, confidence * 100))
---
+++ ex2.py
@@ -0,0 +1,63 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.applications import inception_v3
+from keras import backend as K
+from PIL import Image
+
+# Carga el modelo
+model = inception_v3.InceptionV3()
+
+model_input_layer = model.layers[0].input
+model_output_layer = model.layers[-1].output
+
+object_type_to_fake = 151
+
+# Cargamos imagen
+img = image.load_img("pelican.jpg", target_size=(300, 300))
+original_image = image.img_to_array(img)
+
+# reescalamos los valores de la imagen en el array están entre 1 y -1
+original_image /= 255.
+original_image -= 0.5
+original_image *= 2.
+
+descalado = Image.fromarray(original_image.astype(np.uint8))
+descalado.show()
+#print(original_image)
+
+original_image = np.expand_dims(original_image, axis=0)
+
+# Precalcular cambios
+max_change_above = original_image + 0.01
+max_change_below = original_image - 0.01
+
+# Create a copy of the input image to hack on
+final = np.copy(original_image)
+
+learning_rate = 1.0
+cost_function = model_output_layer[0, object_type_to_fake]
+gradient_function = K.gradients(cost_function, model_input_layer)[0]
+grab_cost_and_gradients_from_model = K.function([model_input_layer, K.learning_phase()], [cost_function, gradient_function])
+cost = 0.0
+
+# Bucle para calcular la aimgen
+while cost < 0.90:
+    cost, gradients = grab_cost_and_gradients_from_model([final, 0])
+    final += gradients * learning_rate
+    final = np.clip(final, max_change_below, max_change_above)
+    final = np.clip(final, -1.0, 1.0)
+
+    #print(grab_cost_and_gradients_from_model)
+    print("Predicción del modelo (chihuahua): {:.8}%".format(cost * 100))
+
+descalado2 = Image.fromarray(final.astype(np.uint8))
+descalado2.show()
+
+# deescalado
+img = final[0]
+img /= 2.
+img += 0.5
+img *= 255.
+
+im = Image.fromarray(img.astype(np.uint8))
+im.save("result.jpg")
